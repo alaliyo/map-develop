ec73bd18e584f5a7fcbd1f8cac9e891d19fb4689
alaliyo <des321321@naver.com>
2023-03-16 23:40:17 +0900

    feat: 세팅!

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,7 +1,11 @@
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
 
 function Root() {
   return (
     <div>
+      <Header />
+      <Outlet />
     </div>
   );
 }
--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from './Root';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Root />,
         children: [
-
-        ]
+            {
+                path: '',
+                element: <Home />,
+            },
+        ],
+        errorElement: <NotFound />
     }
 ])
 
